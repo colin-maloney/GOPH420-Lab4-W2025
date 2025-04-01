a718e7cf9a4968713afd70e25748544550598867
Viktor Pejic <viktor.pejic@ucalgary.ca>
2025-04-01 09:44:35 -0600

    added plots

--- examples/driver.py
+++ examples/driver.py
@@ -26,25 +26,31 @@ def main():
     print(f"R^2: {R2_1}") 
     print(f" residuals: {em1}")
 
+    y_model1 = []
     for i, mag in enumerate(m_data[:int_1]):
-        y_model =
+        y_model1.append(aCoeff1[0] + aCoeff1[1] * mag)
 
 
     int_2 = np.argwhere(t < 45)[-1].item() 
 
-    m2 = np.linspace(-0.9, 1.8, 20) 
+    m2 = np.linspace(-0.9, 1.5, 20)
     n2 = np.zeros_like(m2) 
 
     for i, m2m in enumerate(m2):
         n2[i] = np.count_nonzero(m_data[:int_2] > m2m) 
 
     y2 = np.log(n2) 
-    z2 = np.vstack((np.ones_like(m2), m2)).T 
+    z2 = np.vstack((np.ones_like(m2), m2)).T
 
     aCoeff2, em2, R2_2 = multiregression(y2, z2)
+
     print(f"aCoeff: {aCoeff2}") 
     print(f"R^2: {R2_2}") 
-    print(f" residuals: {em2}") 
+    print(f" residuals: {em2}")
+
+    y_model2 = []
+    for i, mag in enumerate(m_data[:int_2]):
+        y_model2.append(aCoeff2[0] + aCoeff2[1] * mag)
 
     int_3 = np.argwhere(t < 73)[-1].item()
 
@@ -60,16 +66,19 @@ def main():
     aCoeff3, em3, R2_3 = multiregression(y3, z3) 
     print(f"aCoeff: {aCoeff3}")
     print(f"R^2: {R2_3}") 
-    print(f" residuals: {em3}") 
+    print(f" residuals: {em3}")
 
+    y_model3 = []
+    for i, mag in enumerate(m_data[:int_3]):
+        y_model3.append(aCoeff3[0] + aCoeff3[1] * mag)
 
-    int4 = np.argwhere(t < 96)[-1].item()
+    int_4 = np.argwhere(t < 96)[-1].item()
 
     m4 = np.linspace(-0.9, 1.0, 20) 
     n4 = np.zeros_like(m4) 
 
     for i, m4m in enumerate(m4):
-        n4[i] = np.count_nonzero(m_data[:int4] > m4m) 
+        n4[i] = np.count_nonzero(m_data[:int_4] > m4m)
 
     y4 = np.log(n4) 
     z4 = np.vstack((np.ones_like(m4), m4)).T 
@@ -77,15 +86,19 @@ def main():
     aCoeff4, em4, R2_4 = multiregression(y4, z4) 
     print(f"aCoeff: {aCoeff4}")
     print(f"R^2: {R2_4}") 
-    print(f" residuals: {em4}") 
+    print(f" residuals: {em4}")
 
-    int5 = np.argwhere(t < 120)[-1].item() 
+    y_model4 = []
+    for i, mag in enumerate(m_data[:int_4]):
+        y_model4.append(aCoeff4[0] + aCoeff4[1] * mag)
+
+    int_5 = np.argwhere(t < 120)[-1].item()
 
     m5 = np.linspace(-1.1, 1.1, 20) 
     n = np.zeros_like(m5) 
 
     for i, m5m in enumerate(m5):
-        n[i] = np.count_nonzero(m_data[:int5] > m5m) 
+        n[i] = np.count_nonzero(m_data[:int_5] > m5m)
 
     y5 = np.log(n) 
     z5 = np.vstack((np.ones_like(m5), m5)).T 
@@ -93,11 +106,34 @@ def main():
     aCoeff5, em5, R2_5 = multiregression(y5, z5)
     print(f"aCoeff: {aCoeff5}") 
     print(f"R^2: {R2_5}") 
-    print(f" residuals: {em5}") 
-     
+    print(f" residuals: {em5}")
 
-    
-    
+    y_model5 = []
+    for i, mag in enumerate(m_data[:int_5]):
+        y_model5.append(aCoeff5[0] + aCoeff5[1] * mag)
+
+    fig, axes = plt.subplots(1, 5, figsize=(20, 4))
+
+    data_list = [
+        (m_data[:int_1], y_model1, aCoeff1, "0 < t < 35hrs"),
+        (m_data[:int_2], y_model2, aCoeff2, "35hrs < t < 45hrs"),
+        (m_data[:int_3], y_model3, aCoeff3, "45hrs < t < 73hrs"),
+        (m_data[:int_4], y_model4, aCoeff4, "73hrs < t < 96hrs"),
+        (m_data[:int_5], y_model5, aCoeff5, "96hrs < t < 120hrs"),
+    ]
+
+    for ax, (x_data, y_model, aCoeff, title) in zip(axes, data_list):
+        ax.plot(x_data, y_model, label=f"y = {aCoeff[0]:.2f} + {aCoeff[1]:.2f}m")
+        ax.set_xlabel("m_data")
+        ax.set_ylabel("y_model")
+        ax.set_title(title)
+        ax.legend()
+        ax.grid()
+
+    plt.tight_layout()
+    plt.savefig("../figures/regression_lines.png")
+
+
 if __name__ == "__main__":
     main()
 
