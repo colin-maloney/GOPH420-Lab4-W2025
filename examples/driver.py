a663085da1459899a29b0758f90067d50a473480
Viktor Pejic <viktor.pejic@ucalgary.ca>
2025-04-01 09:10:24 -0600

    fixed first interval

--- examples/driver.py
+++ examples/driver.py
@@ -3,18 +3,18 @@ import numpy as np
 from src.goph_lab04.regression import multiregression 
 
 def main(): 
-    data = np.leatxt(".../data/M_data1.txt") 
+    data = np.loadtxt("../data/M_data1.txt")
 
     t = data[:, 0] 
     m_data = data[:, 1] 
 
-    int_1 = np.argwhere(t < 35)[-1] 
+    int_1 = np.argwhere(t < 35)[-1].item()
 
-    m = np.linespace(-0.5, 1.5, 19) 
-    n = np.zeros_like(m) 
+    m = np.linspace(-0.2, 1.0, 20)
+    n = np.zeros_like(m)
 
     for i, mm in enumerate(m): 
-        n[i] = np.sum(m_data[int_1:] > mm)
+        n[i] = np.count_nonzero(m_data[:int_1] > mm)
 
     y = np.log(n) 
     z = np.vstack((np.ones_like(m), m)).T 
@@ -24,3 +24,7 @@ def main():
     print(f"aCoeff: {aCoeff}")
     print(f"R^2: {R2}") 
     print(f" residuals: {em}")
+
+if __name__ == "__main__":
+    main()
+
